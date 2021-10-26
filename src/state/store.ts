29668db55f6d5ba3bc726e93989f6769b8ab3ab6
Oluwafemi <akinsiku13@gmail.com>
2021-10-26 01:46:40 +0100

    remove unused dependencies

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,18 +1,10 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import reducers from './reducers';
-
-// export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 
 
-const middlewares = [logger, thunk]
+const middlewares = [ thunk]
 
 export const store = createStore(reducers, {}, applyMiddleware(...middlewares));
 
